0c67556092b32d5da66c6e38c7d1a742e807db6c
IlyasDiker <ilyasbenhssine@gmail.com>
2020-05-25 18:13:46 +0000

    add base

---
+++ Form1.Designer.vb
@@ -0,0 +1,229 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Launcher
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Launcher))
+		Me.Button2 = New System.Windows.Forms.Button()
+		Me.Button1 = New System.Windows.Forms.Button()
+		Me.Button3 = New System.Windows.Forms.Button()
+		Me.Button4 = New System.Windows.Forms.Button()
+		Me.gotoTS = New System.Windows.Forms.Button()
+		Me.GotoWeb = New System.Windows.Forms.Button()
+		Me.Beta = New System.Windows.Forms.Label()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.wiki = New System.Windows.Forms.Button()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.SuspendLayout()
+		'
+		'Button2
+		'
+		Me.Button2.BackColor = System.Drawing.Color.Transparent
+		Me.Button2.BackgroundImage = CType(resources.GetObject("Button2.BackgroundImage"), System.Drawing.Image)
+		Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+		Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+		Me.Button2.FlatAppearance.BorderSize = 0
+		Me.Button2.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.MenuHighlight
+		Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+		Me.Button2.Location = New System.Drawing.Point(807, 7)
+		Me.Button2.Name = "Button2"
+		Me.Button2.Size = New System.Drawing.Size(40, 36)
+		Me.Button2.TabIndex = 3
+		Me.Button2.UseVisualStyleBackColor = False
+		'
+		'Button1
+		'
+		Me.Button1.BackColor = System.Drawing.Color.Transparent
+		Me.Button1.BackgroundImage = CType(resources.GetObject("Button1.BackgroundImage"), System.Drawing.Image)
+		Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+		Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+		Me.Button1.FlatAppearance.BorderSize = 0
+		Me.Button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon
+		Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+		Me.Button1.ForeColor = System.Drawing.SystemColors.ControlText
+		Me.Button1.Location = New System.Drawing.Point(852, 7)
+		Me.Button1.Margin = New System.Windows.Forms.Padding(5)
+		Me.Button1.Name = "Button1"
+		Me.Button1.Size = New System.Drawing.Size(40, 36)
+		Me.Button1.TabIndex = 2
+		Me.Button1.UseVisualStyleBackColor = False
+		'
+		'Button3
+		'
+		Me.Button3.BackColor = System.Drawing.Color.Transparent
+		Me.Button3.BackgroundImage = CType(resources.GetObject("Button3.BackgroundImage"), System.Drawing.Image)
+		Me.Button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+		Me.Button3.Cursor = System.Windows.Forms.Cursors.Hand
+		Me.Button3.FlatAppearance.BorderSize = 0
+		Me.Button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+		Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+		Me.Button3.Font = New System.Drawing.Font("Franklin Gothic Medium", 19.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Button3.ForeColor = System.Drawing.Color.Transparent
+		Me.Button3.Location = New System.Drawing.Point(36, 188)
+		Me.Button3.Margin = New System.Windows.Forms.Padding(0)
+		Me.Button3.Name = "Button3"
+		Me.Button3.Size = New System.Drawing.Size(400, 201)
+		Me.Button3.TabIndex = 6
+		Me.Button3.UseVisualStyleBackColor = False
+		'
+		'Button4
+		'
+		Me.Button4.BackColor = System.Drawing.Color.Transparent
+		Me.Button4.BackgroundImage = CType(resources.GetObject("Button4.BackgroundImage"), System.Drawing.Image)
+		Me.Button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+		Me.Button4.Cursor = System.Windows.Forms.Cursors.Hand
+		Me.Button4.FlatAppearance.BorderSize = 0
+		Me.Button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+		Me.Button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+		Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+		Me.Button4.ForeColor = System.Drawing.Color.Transparent
+		Me.Button4.Location = New System.Drawing.Point(36, 441)
+		Me.Button4.Name = "Button4"
+		Me.Button4.Size = New System.Drawing.Size(48, 39)
+		Me.Button4.TabIndex = 7
+		Me.Button4.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+		Me.Button4.UseVisualStyleBackColor = False
+		'
+		'gotoTS
+		'
+		Me.gotoTS.BackColor = System.Drawing.Color.Transparent
+		Me.gotoTS.BackgroundImage = CType(resources.GetObject("gotoTS.BackgroundImage"), System.Drawing.Image)
+		Me.gotoTS.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+		Me.gotoTS.Cursor = System.Windows.Forms.Cursors.Hand
+		Me.gotoTS.FlatAppearance.BorderSize = 0
+		Me.gotoTS.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+		Me.gotoTS.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+		Me.gotoTS.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+		Me.gotoTS.ForeColor = System.Drawing.Color.Transparent
+		Me.gotoTS.Location = New System.Drawing.Point(474, 188)
+		Me.gotoTS.Margin = New System.Windows.Forms.Padding(0)
+		Me.gotoTS.Name = "gotoTS"
+		Me.gotoTS.Size = New System.Drawing.Size(400, 201)
+		Me.gotoTS.TabIndex = 9
+		Me.gotoTS.UseVisualStyleBackColor = False
+		'
+		'GotoWeb
+		'
+		Me.GotoWeb.BackColor = System.Drawing.Color.Transparent
+		Me.GotoWeb.BackgroundImage = CType(resources.GetObject("GotoWeb.BackgroundImage"), System.Drawing.Image)
+		Me.GotoWeb.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+		Me.GotoWeb.Cursor = System.Windows.Forms.Cursors.Hand
+		Me.GotoWeb.FlatAppearance.BorderSize = 0
+		Me.GotoWeb.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+		Me.GotoWeb.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+		Me.GotoWeb.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+		Me.GotoWeb.ForeColor = System.Drawing.Color.Transparent
+		Me.GotoWeb.Location = New System.Drawing.Point(29, 10)
+		Me.GotoWeb.Name = "GotoWeb"
+		Me.GotoWeb.Size = New System.Drawing.Size(34, 30)
+		Me.GotoWeb.TabIndex = 10
+		Me.GotoWeb.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+		Me.GotoWeb.UseVisualStyleBackColor = False
+		'
+		'Beta
+		'
+		Me.Beta.AutoSize = True
+		Me.Beta.BackColor = System.Drawing.Color.Transparent
+		Me.Beta.ForeColor = System.Drawing.SystemColors.ButtonFace
+		Me.Beta.Location = New System.Drawing.Point(61, 10)
+		Me.Beta.Name = "Beta"
+		Me.Beta.Size = New System.Drawing.Size(29, 13)
+		Me.Beta.TabIndex = 11
+		Me.Beta.Text = "Beta"
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.BackColor = System.Drawing.Color.Transparent
+		Me.Label1.Font = New System.Drawing.Font("Segoe UI", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonFace
+		Me.Label1.Location = New System.Drawing.Point(35, 81)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(157, 50)
+		Me.Label1.TabIndex = 12
+		Me.Label1.Text = "ALTTAB"
+		'
+		'wiki
+		'
+		Me.wiki.BackColor = System.Drawing.Color.Transparent
+		Me.wiki.BackgroundImage = CType(resources.GetObject("wiki.BackgroundImage"), System.Drawing.Image)
+		Me.wiki.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+		Me.wiki.FlatAppearance.BorderSize = 0
+		Me.wiki.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+		Me.wiki.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.wiki.ForeColor = System.Drawing.Color.Transparent
+		Me.wiki.Location = New System.Drawing.Point(91, 441)
+		Me.wiki.Name = "wiki"
+		Me.wiki.Size = New System.Drawing.Size(70, 39)
+		Me.wiki.TabIndex = 13
+		Me.wiki.UseVisualStyleBackColor = False
+		'
+		'Label2
+		'
+		Me.Label2.AutoSize = True
+		Me.Label2.BackColor = System.Drawing.SystemColors.HotTrack
+		Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label2.ForeColor = System.Drawing.SystemColors.ButtonFace
+		Me.Label2.Location = New System.Drawing.Point(188, 92)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(33, 13)
+		Me.Label2.TabIndex = 14
+		Me.Label2.Text = "Beta"
+		'
+		'Launcher
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+		Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+		Me.ClientSize = New System.Drawing.Size(902, 506)
+		Me.Controls.Add(Me.Label2)
+		Me.Controls.Add(Me.wiki)
+		Me.Controls.Add(Me.Label1)
+		Me.Controls.Add(Me.Beta)
+		Me.Controls.Add(Me.GotoWeb)
+		Me.Controls.Add(Me.gotoTS)
+		Me.Controls.Add(Me.Button4)
+		Me.Controls.Add(Me.Button3)
+		Me.Controls.Add(Me.Button2)
+		Me.Controls.Add(Me.Button1)
+		Me.DoubleBuffered = True
+		Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+		Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+		Me.Name = "Launcher"
+		Me.Text = "ALTTAB Beta"
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
+
+	End Sub
+
+	Friend WithEvents Button2 As Button
+	Friend WithEvents Button1 As Button
+	Friend WithEvents Button3 As Button
+	Friend WithEvents Button4 As Button
+	Friend WithEvents gotoTS As Button
+	Friend WithEvents GotoWeb As Button
+	Friend WithEvents Beta As Label
+	Friend WithEvents Label1 As Label
+	Friend WithEvents wiki As Button
+	Friend WithEvents Label2 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,51 @@
+﻿
+Public Class Launcher
+    Dim drag As Boolean
+    Dim mousex As Integer
+    Dim mousey As Integer
+
+    Private Sub Launcher_MouseDown(sender As Object, e As MouseEventArgs) Handles MyBase.MouseDown
+        drag = True
+        mousex = Cursor.Position.X - Me.Left
+        mousey = Cursor.Position.Y - Me.Top
+    End Sub
+
+    Private Sub Launcher_MouseMove(sender As Object, e As MouseEventArgs) Handles MyBase.MouseMove
+        If drag Then
+            Me.Top = Cursor.Position.Y - mousey
+            Me.Left = Cursor.Position.X - mousex
+        End If
+    End Sub
+
+    Private Sub Launcher_MouseUp(sender As Object, e As MouseEventArgs) Handles MyBase.MouseUp
+        drag = False
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Shell("Explorer.exe fivem://connect/XXX.XXX.XXX.XXX:30120")
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Shell("Explorer.exe https://discord.com/invite/XXXXXXX")
+    End Sub
+
+    Private Sub gotoTS_Click(sender As Object, e As EventArgs) Handles gotoTS.Click
+        Shell("Explorer.exe ts3server://ts.XXXXXXXXX.com")
+    End Sub
+
+    Private Sub GotoWeb_Click(sender As Object, e As EventArgs) Handles GotoWeb.Click
+        Shell("Explorer.exe https://www.alttabgaming.com/")
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
